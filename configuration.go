188779fe6405df1320ed901e8425c2e556ce54df
David Kua <david@kua.io>
2014-02-16 02:44:46 -0500

    Added Route struct from documentation.
    Route also includes Stop, Direction, Path, and Point.

--- configuration.go
+++ configuration.go
@@ -6,3 +6,45 @@ type Agency struct {
 	RegionTitle string `xml: "regionTitle,attr"`
 	ShortTitle  string `xml: "shortTitle,attr"`
 }
+
+type Route struct {
+	Tag           string  `xml: "tag,attr"`
+	Title         string  `xml: "title,attr"`
+	ShortTitle    string  `xml: "shortTitle,attr"`
+	Color         string  `xml: "color,attr"`
+	OppositeColor string  `xml: "oppositeColor,attr"`
+	LatMin        float32 `xml: "latMin,attr"`
+	LatMax        float32 `xml: "latMax,attr"`
+	LonMin        float32 `xml: "lonMin,attr"`
+	LonMax        float32 `xml: "lonMax,attr"`
+
+	Stops      []Stop
+	Directions []Direction
+	Paths      []Path
+}
+
+type Stop struct {
+	Tag        string  `xml: "tag,attr"`
+	Title      string  `xml: "title,attr"`
+	ShortTitle string  `xml: "shortTitle,attr"`
+	Lat        float32 `xml: "lat,attr"`
+	Lon        float32 `xml: "lon,attr"`
+	StopId     int     `xml: "stopId,attr"`
+}
+
+type Direction struct {
+	Tag      string `xml: "tag,attr"`
+	Title    string `xml: "title,attr"`
+	Name     string `xml: "name,attr"`
+	UseForUI bool   `xml: "useForUI,attr"`
+	Stops    []Stop
+}
+
+type Path struct {
+	Points []Point
+}
+
+type Point struct {
+	Lat float32 `xml: "lat,attr"`
+	Lon float32 `xml: "lon,attr"`
+}
